23a164a5ae4c90903ca828b8009fc91a26a280e8
Doug Hellmann <doug@doughellmann.com>
2014-02-06 07:45:46 -0500

    update to pbr

--- HISTORY.rst
+++
@@ -1,9 +0,0 @@
-.. :changelog:
-
-History
--------
-
-0.1.0 (2014-02-06)
-++++++++++++++++++
-
-* First release on PyPI.
--- setup.py
+++ setup.py
@@ -1,50 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import setuptools
 
-import os
-import sys
-
-
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    from setuptools import setup
+    import multiprocessing  # flake8: noqa
 except ImportError:
-    from distutils.core import setup
+    pass
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-
-setup(
-    name='whatthewhat',
-    version='0.1.0',
-    description='Launch a Google search for exceptions from Python apps',
-    long_description=readme + '\n\n' + history,
-    author='Doug Hellmann',
-    author_email='doug@doughellmann.com',
-    url='https://github.com/dhellmann/whatthewhat',
-    packages=[
-        'whatthewhat',
-    ],
-    package_dir={'whatthewhat': 'whatthewhat'},
-    include_package_data=True,
-    install_requires=[
-    ],
-    license="BSD",
-    zip_safe=False,
-    keywords='whatthewhat',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-    ],
-    test_suite='tests',
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True,
 )
